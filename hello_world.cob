a871cd32927cc3b8f390e6ab30a559bb97a737cc
fernandodatageo <fernando.datageo@gmail.com>
2022-07-31 14:53:06 -0300

    Create hello_world.cob

---
+++ hello_world.cob
@@ -0,0 +1,13 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. PROGCOB01.
+      *********************************************
+      *    AREA DE COMENTÁRIOS - REMARKS
+      *    AUTHOR  = LUIS FERNANDO
+      *    OBJETIVO: MOSTRAR UMA MSG DE HELLO WORLD
+      *    DATA    = 01/06/2021
+      *********************************************
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       PROCEDURE DIVISION.
+           DISPLAY 'Hello World!'.
+           STOP RUN.
